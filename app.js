8d577e05fbc0fb2e9a91e794474c5fceb414c6a3
z-e-n-d <zozo.toth.2021home@gmail.com>
2025-01-24 21:11:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,11 @@ app.post('/login', (req, res) => {
     res.status(200).json({ username: user.username });
 });
 
+// Keep server awake (ping endpoint)
+app.get('/ping', (req, res) => {
+    res.status(200).send('Server is awake');
+});
+
 // Start server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
