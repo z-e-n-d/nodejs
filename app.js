625712804627ffd3b2d3d6e8ea63363ccc597ac8
z-e-n-d <zozo.toth.2021home@gmail.com>
2025-01-25 14:57:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(express.json());
